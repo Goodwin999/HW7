25dffd1f218e19719443998fe0cde06b102a759e
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2023-09-11 22:33:52 +0300

    Fix task 1/выполнил 1 задачу

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        int contribution = 15000;
+        int total = 0;
+        int i =  0;
+        while (total < 2_459_000) {
+            total = total + contribution;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total);
+            i = i + 1;
+        }
+
+
+
     }
 }
